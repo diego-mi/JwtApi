5a06786b4d31ec734a370b05fcaa4a6f11f44ad3
Diego Mi Campos <diegomister@gmail.com>
2019-01-13 14:15:43 -0200

    adicionado controller de planejamento

---
+++ JwtAuthentication/Controllers/PlanejamentosController.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using JwtAuthentication.Data;
+using JwtAuthentication.Entities.Planejamentos;
+
+namespace JwtAuthentication.Controllers
+{
+    [Route("api/v1/planejamentos")]
+    [ApiController]
+    public class PlanejamentosController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public PlanejamentosController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Planejamentos
+        [HttpGet]
+        public IEnumerable<Planejamento> GetPlanejamentos()
+        {
+            return _context.Planejamentos;
+        }
+
+        // GET: api/Planejamentos/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetPlanejamento([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var planejamento = await _context.Planejamentos.FindAsync(id);
+
+            if (planejamento == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(planejamento);
+        }
+
+        // PUT: api/Planejamentos/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutPlanejamento([FromRoute] int id, [FromBody] Planejamento planejamento)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != planejamento.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(planejamento).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PlanejamentoExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Planejamentos
+        [HttpPost]
+        public async Task<IActionResult> PostPlanejamento([FromBody] Planejamento planejamento)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.Planejamentos.Add(planejamento);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetPlanejamento", new { id = planejamento.Id }, planejamento);
+        }
+
+        // DELETE: api/Planejamentos/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeletePlanejamento([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var planejamento = await _context.Planejamentos.FindAsync(id);
+            if (planejamento == null)
+            {
+                return NotFound();
+            }
+
+            _context.Planejamentos.Remove(planejamento);
+            await _context.SaveChangesAsync();
+
+            return Ok(planejamento);
+        }
+
+        private bool PlanejamentoExists(int id)
+        {
+            return _context.Planejamentos.Any(e => e.Id == id);
+        }
+    }
+}
