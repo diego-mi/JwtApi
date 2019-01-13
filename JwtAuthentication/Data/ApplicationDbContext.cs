3c6ff0582a9427c15b53521fe27d50c663c5c91e
Diego Mi Campos <diegomister@gmail.com>
2019-01-13 14:04:17 -0200

    Adicionado seed de categorias

--- JwtAuthentication/Data/ApplicationDbContext.cs
+++ JwtAuthentication/Data/ApplicationDbContext.cs
@@ -36,6 +36,68 @@ namespace JwtAuthentication.Data
                 new { Id = "2", Name = "Customer", NormalizedName = "CUSTOMER" }
             );
 
+            builder.Entity<Categoria>().HasData(
+                // Categorizar
+                new Categoria() { Id = 1, Nome = "A Categorizar", Grupo = CategoriaGrupoEnum.NaoCategorizado, AutorId = null },
+                // Contas Residenciais
+                new Categoria() { Id = 2, Nome = "Aluguel", Grupo = CategoriaGrupoEnum.ContasResidenciais, AutorId = null },
+                new Categoria() { Id = 3, Nome = "Luz", Grupo = CategoriaGrupoEnum.ContasResidenciais, AutorId = null },
+                new Categoria() { Id = 4, Nome = "Água", Grupo = CategoriaGrupoEnum.ContasResidenciais, AutorId = null },
+                new Categoria() { Id = 5, Nome = "TV/Internet/Telefone Residêncial", Grupo = CategoriaGrupoEnum.ContasResidenciais, AutorId = null },
+                new Categoria() { Id = 6, Nome = "Manutenção/Melhorias", Grupo = CategoriaGrupoEnum.ContasResidenciais, AutorId = null },
+                new Categoria() { Id = 7, Nome = "Seguros", Grupo = CategoriaGrupoEnum.ContasResidenciais, AutorId = null },
+                new Categoria() { Id = 8, Nome = "Segurança", Grupo = CategoriaGrupoEnum.ContasResidenciais, AutorId = null },
+                // Alimentação
+                new Categoria() { Id = 9, Nome = "Mercado Dispensa/Essencial", Grupo = CategoriaGrupoEnum.Alimentacao, AutorId = null },
+                new Categoria() { Id = 10, Nome = "Mercado Filhas", Grupo = CategoriaGrupoEnum.Alimentacao, AutorId = null },
+                new Categoria() { Id = 11, Nome = "Mercado não essencial", Grupo = CategoriaGrupoEnum.Alimentacao, AutorId = null },
+                // Saúde
+                new Categoria() { Id = 12, Nome = "Higiêne Geral", Grupo = CategoriaGrupoEnum.Saude, AutorId = null },
+                new Categoria() { Id = 13, Nome = "Higiêne Filhas", Grupo = CategoriaGrupoEnum.Saude, AutorId = null },
+                new Categoria() { Id = 14, Nome = "Plano de saúde", Grupo = CategoriaGrupoEnum.Saude, AutorId = null },
+                new Categoria() { Id = 15, Nome = "Farmácia", Grupo = CategoriaGrupoEnum.Saude, AutorId = null },
+                new Categoria() { Id = 16, Nome = "Farmácia Filhas", Grupo = CategoriaGrupoEnum.Saude, AutorId = null },
+                // Transporte
+                new Categoria() { Id = 17, Nome = "Moto - Abastecimento", Grupo = CategoriaGrupoEnum.Transporte, AutorId = null },
+                new Categoria() { Id = 18, Nome = "Moto - Manutenção/Melhorias", Grupo = CategoriaGrupoEnum.Transporte, AutorId = null },
+                new Categoria() { Id = 19, Nome = "Uber - Essencial", Grupo = CategoriaGrupoEnum.Transporte, AutorId = null },
+                new Categoria() { Id = 20, Nome = "Uber - Escola Laura", Grupo = CategoriaGrupoEnum.Transporte, AutorId = null },
+                new Categoria() { Id = 21, Nome = "Uber - Saidinhas e Outros", Grupo = CategoriaGrupoEnum.Transporte, AutorId = null },
+                new Categoria() { Id = 22, Nome = "Outros", Grupo = CategoriaGrupoEnum.Transporte, AutorId = null },
+                // Gastos Essenciais
+                new Categoria() { Id = 23, Nome = "Educação", Grupo = CategoriaGrupoEnum.GastosEssenciais, AutorId = null },
+                // Gastos Gerais
+                new Categoria() { Id = 24, Nome = "Lanches / Saidinhas", Grupo = CategoriaGrupoEnum.GastosGerais, AutorId = null },
+                new Categoria() { Id = 25, Nome = "Compras", Grupo = CategoriaGrupoEnum.GastosGerais, AutorId = null },
+                new Categoria() { Id = 26, Nome = "Cuidados Pessoais", Grupo = CategoriaGrupoEnum.GastosGerais, AutorId = null },
+                new Categoria() { Id = 27, Nome = "Despesas do Trabalho", Grupo = CategoriaGrupoEnum.GastosGerais, AutorId = null },
+                new Categoria() { Id = 28, Nome = "Presentes/Doações", Grupo = CategoriaGrupoEnum.GastosGerais, AutorId = null },
+                // Familia
+                new Categoria() { Id = 29, Nome = "Pensão Mariah", Grupo = CategoriaGrupoEnum.Familia, AutorId = null },
+                // Servicos Bancários
+                new Categoria() { Id = 30, Nome = "Impostos", Grupo = CategoriaGrupoEnum.ServicosBancarios, AutorId = null },
+                new Categoria() { Id = 31, Nome = "Multa/Juros", Grupo = CategoriaGrupoEnum.ServicosBancarios, AutorId = null },
+                new Categoria() { Id = 32, Nome = "Taxas Bancárias", Grupo = CategoriaGrupoEnum.ServicosBancarios, AutorId = null },
+                new Categoria() { Id = 33, Nome = "Cheque Especial", Grupo = CategoriaGrupoEnum.ServicosBancarios, AutorId = null },
+                new Categoria() { Id = 34, Nome = "Pagamento de Empréstimo", Grupo = CategoriaGrupoEnum.ServicosBancarios, AutorId = null },
+                // Renda
+                new Categoria() { Id = 35, Nome = "Remuneração", Grupo = CategoriaGrupoEnum.Renda, AutorId = null },
+                new Categoria() { Id = 36, Nome = "Bônus", Grupo = CategoriaGrupoEnum.Renda, AutorId = null },
+                new Categoria() { Id = 37, Nome = "Rendimento", Grupo = CategoriaGrupoEnum.Renda, AutorId = null },
+                new Categoria() { Id = 38, Nome = "Empréstimo", Grupo = CategoriaGrupoEnum.Renda, AutorId = null },
+                new Categoria() { Id = 48, Nome = "Vale Refeição", Grupo = CategoriaGrupoEnum.Renda, AutorId = null },
+                new Categoria() { Id = 39, Nome = "Outras Rendas", Grupo = CategoriaGrupoEnum.Renda, AutorId = null },
+                new Categoria() { Id = 40, Nome = "Indenização - Despesa do Trabalho", Grupo = CategoriaGrupoEnum.Renda, AutorId = null },
+                // Movimentação entre contas
+                new Categoria() { Id = 42, Nome = "Saque", Grupo = CategoriaGrupoEnum.MovimentacaoEntreContas, AutorId = null },
+                new Categoria() { Id = 43, Nome = "Aplicação", Grupo = CategoriaGrupoEnum.MovimentacaoEntreContas, AutorId = null },
+                new Categoria() { Id = 44, Nome = "Resgate", Grupo = CategoriaGrupoEnum.MovimentacaoEntreContas, AutorId = null },
+                new Categoria() { Id = 45, Nome = "Transfência", Grupo = CategoriaGrupoEnum.MovimentacaoEntreContas, AutorId = null },
+                new Categoria() { Id = 46, Nome = "Pagamento de cartão", Grupo = CategoriaGrupoEnum.MovimentacaoEntreContas, AutorId = null },
+                // Lançamentos subdivididos
+                new Categoria() { Id = 47, Nome = "Lançamento subcategorizado", Grupo = CategoriaGrupoEnum.Registro, AutorId = null }
+            );
+
             #endregion
         }
     }
---
+++ JwtAuthentication/Migrations/20190113160351_SeedCategory.cs
@@ -0,0 +1,302 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace JwtAuthentication.Migrations
+{
+    public partial class SeedCategory : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Categorias",
+                columns: new[] { "Id", "AutorId", "Grupo", "Nome" },
+                values: new object[,]
+                {
+                    { 1, null, 0, "A Categorizar" },
+                    { 26, null, 6, "Cuidados Pessoais" },
+                    { 27, null, 6, "Despesas do Trabalho" },
+                    { 28, null, 6, "Presentes/Doações" },
+                    { 29, null, 7, "Pensão Mariah" },
+                    { 30, null, 8, "Impostos" },
+                    { 31, null, 8, "Multa/Juros" },
+                    { 32, null, 8, "Taxas Bancárias" },
+                    { 33, null, 8, "Cheque Especial" },
+                    { 34, null, 8, "Pagamento de Empréstimo" },
+                    { 25, null, 6, "Compras" },
+                    { 35, null, 9, "Remuneração" },
+                    { 37, null, 9, "Rendimento" },
+                    { 38, null, 9, "Empréstimo" },
+                    { 48, null, 9, "Vale Refeição" },
+                    { 39, null, 9, "Outras Rendas" },
+                    { 40, null, 9, "Indenização - Despesa do Trabalho" },
+                    { 42, null, 10, "Saque" },
+                    { 43, null, 10, "Aplicação" },
+                    { 44, null, 10, "Resgate" },
+                    { 45, null, 10, "Transfência" },
+                    { 36, null, 9, "Bônus" },
+                    { 46, null, 10, "Pagamento de cartão" },
+                    { 24, null, 6, "Lanches / Saidinhas" },
+                    { 22, null, 5, "Outros" },
+                    { 2, null, 1, "Aluguel" },
+                    { 3, null, 1, "Luz" },
+                    { 4, null, 1, "Água" },
+                    { 5, null, 1, "TV/Internet/Telefone Residêncial" },
+                    { 6, null, 1, "Manutenção/Melhorias" },
+                    { 7, null, 1, "Seguros" },
+                    { 8, null, 1, "Segurança" },
+                    { 9, null, 2, "Mercado Dispensa/Essencial" },
+                    { 10, null, 2, "Mercado Filhas" },
+                    { 23, null, 4, "Educação" },
+                    { 11, null, 2, "Mercado não essencial" },
+                    { 13, null, 3, "Higiêne Filhas" },
+                    { 14, null, 3, "Plano de saúde" },
+                    { 15, null, 3, "Farmácia" },
+                    { 16, null, 3, "Farmácia Filhas" },
+                    { 17, null, 5, "Moto - Abastecimento" },
+                    { 18, null, 5, "Moto - Manutenção/Melhorias" },
+                    { 19, null, 5, "Uber - Essencial" },
+                    { 20, null, 5, "Uber - Escola Laura" },
+                    { 21, null, 5, "Uber - Saidinhas e Outros" },
+                    { 12, null, 3, "Higiêne Geral" },
+                    { 47, null, 11, "Lançamento subcategorizado" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Categorias",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Categorias",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Categorias",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Categorias",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Categorias",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Categorias",
+                keyColumn: "Id",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Categorias",
+                keyColumn: "Id",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "Categorias",
+                keyColumn: "Id",
+                keyValue: 8);
+
+            migrationBuilder.DeleteData(
+                table: "Categorias",
+                keyColumn: "Id",
+                keyValue: 9);
+
+            migrationBuilder.DeleteData(
+                table: "Categorias",
+                keyColumn: "Id",
+                keyValue: 10);
+
+            migrationBuilder.DeleteData(
+                table: "Categorias",
+                keyColumn: "Id",
+                keyValue: 11);
+
+            migrationBuilder.DeleteData(
+                table: "Categorias",
+                keyColumn: "Id",
+                keyValue: 12);
+
+            migrationBuilder.DeleteData(
+                table: "Categorias",
+                keyColumn: "Id",
+                keyValue: 13);
+
+            migrationBuilder.DeleteData(
+                table: "Categorias",
+                keyColumn: "Id",
+                keyValue: 14);
+
+            migrationBuilder.DeleteData(
+                table: "Categorias",
+                keyColumn: "Id",
+                keyValue: 15);
+
+            migrationBuilder.DeleteData(
+                table: "Categorias",
+                keyColumn: "Id",
+                keyValue: 16);
+
+            migrationBuilder.DeleteData(
+                table: "Categorias",
+                keyColumn: "Id",
+                keyValue: 17);
+
+            migrationBuilder.DeleteData(
+                table: "Categorias",
+                keyColumn: "Id",
+                keyValue: 18);
+
+            migrationBuilder.DeleteData(
+                table: "Categorias",
+                keyColumn: "Id",
+                keyValue: 19);
+
+            migrationBuilder.DeleteData(
+                table: "Categorias",
+                keyColumn: "Id",
+                keyValue: 20);
+
+            migrationBuilder.DeleteData(
+                table: "Categorias",
+                keyColumn: "Id",
+                keyValue: 21);
+
+            migrationBuilder.DeleteData(
+                table: "Categorias",
+                keyColumn: "Id",
+                keyValue: 22);
+
+            migrationBuilder.DeleteData(
+                table: "Categorias",
+                keyColumn: "Id",
+                keyValue: 23);
+
+            migrationBuilder.DeleteData(
+                table: "Categorias",
+                keyColumn: "Id",
+                keyValue: 24);
+
+            migrationBuilder.DeleteData(
+                table: "Categorias",
+                keyColumn: "Id",
+                keyValue: 25);
+
+            migrationBuilder.DeleteData(
+                table: "Categorias",
+                keyColumn: "Id",
+                keyValue: 26);
+
+            migrationBuilder.DeleteData(
+                table: "Categorias",
+                keyColumn: "Id",
+                keyValue: 27);
+
+            migrationBuilder.DeleteData(
+                table: "Categorias",
+                keyColumn: "Id",
+                keyValue: 28);
+
+            migrationBuilder.DeleteData(
+                table: "Categorias",
+                keyColumn: "Id",
+                keyValue: 29);
+
+            migrationBuilder.DeleteData(
+                table: "Categorias",
+                keyColumn: "Id",
+                keyValue: 30);
+
+            migrationBuilder.DeleteData(
+                table: "Categorias",
+                keyColumn: "Id",
+                keyValue: 31);
+
+            migrationBuilder.DeleteData(
+                table: "Categorias",
+                keyColumn: "Id",
+                keyValue: 32);
+
+            migrationBuilder.DeleteData(
+                table: "Categorias",
+                keyColumn: "Id",
+                keyValue: 33);
+
+            migrationBuilder.DeleteData(
+                table: "Categorias",
+                keyColumn: "Id",
+                keyValue: 34);
+
+            migrationBuilder.DeleteData(
+                table: "Categorias",
+                keyColumn: "Id",
+                keyValue: 35);
+
+            migrationBuilder.DeleteData(
+                table: "Categorias",
+                keyColumn: "Id",
+                keyValue: 36);
+
+            migrationBuilder.DeleteData(
+                table: "Categorias",
+                keyColumn: "Id",
+                keyValue: 37);
+
+            migrationBuilder.DeleteData(
+                table: "Categorias",
+                keyColumn: "Id",
+                keyValue: 38);
+
+            migrationBuilder.DeleteData(
+                table: "Categorias",
+                keyColumn: "Id",
+                keyValue: 39);
+
+            migrationBuilder.DeleteData(
+                table: "Categorias",
+                keyColumn: "Id",
+                keyValue: 40);
+
+            migrationBuilder.DeleteData(
+                table: "Categorias",
+                keyColumn: "Id",
+                keyValue: 42);
+
+            migrationBuilder.DeleteData(
+                table: "Categorias",
+                keyColumn: "Id",
+                keyValue: 43);
+
+            migrationBuilder.DeleteData(
+                table: "Categorias",
+                keyColumn: "Id",
+                keyValue: 44);
+
+            migrationBuilder.DeleteData(
+                table: "Categorias",
+                keyColumn: "Id",
+                keyValue: 45);
+
+            migrationBuilder.DeleteData(
+                table: "Categorias",
+                keyColumn: "Id",
+                keyValue: 46);
+
+            migrationBuilder.DeleteData(
+                table: "Categorias",
+                keyColumn: "Id",
+                keyValue: 47);
+
+            migrationBuilder.DeleteData(
+                table: "Categorias",
+                keyColumn: "Id",
+                keyValue: 48);
+        }
+    }
+}
