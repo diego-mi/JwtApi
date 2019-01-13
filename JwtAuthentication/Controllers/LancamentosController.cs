1113250fb5ff262fd47a46250ba768404b06ff13
Diego Mi Campos <diegomister@gmail.com>
2019-01-13 14:14:20 -0200

    adicionado lancamentoscontroller

---
+++ JwtAuthentication/Controllers/LancamentosController.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using JwtAuthentication.Data;
+using JwtAuthentication.Entities.Lancamentos;
+
+namespace JwtAuthentication.Controllers
+{
+    [Route("api/v1/lancamentos")]
+    [ApiController]
+    public class LancamentosController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public LancamentosController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Lancamentos
+        [HttpGet]
+        public IEnumerable<Lancamento> GetLancamentos()
+        {
+            return _context.Lancamentos;
+        }
+
+        // GET: api/Lancamentos/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetLancamento([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var lancamento = await _context.Lancamentos.FindAsync(id);
+
+            if (lancamento == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(lancamento);
+        }
+
+        // PUT: api/Lancamentos/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutLancamento([FromRoute] int id, [FromBody] Lancamento lancamento)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != lancamento.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(lancamento).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!LancamentoExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Lancamentos
+        [HttpPost]
+        public async Task<IActionResult> PostLancamento([FromBody] Lancamento lancamento)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.Lancamentos.Add(lancamento);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetLancamento", new { id = lancamento.Id }, lancamento);
+        }
+
+        // DELETE: api/Lancamentos/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteLancamento([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var lancamento = await _context.Lancamentos.FindAsync(id);
+            if (lancamento == null)
+            {
+                return NotFound();
+            }
+
+            _context.Lancamentos.Remove(lancamento);
+            await _context.SaveChangesAsync();
+
+            return Ok(lancamento);
+        }
+
+        private bool LancamentoExists(int id)
+        {
+            return _context.Lancamentos.Any(e => e.Id == id);
+        }
+    }
+}
